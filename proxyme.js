c19787888ff3cd844626185106ba6cf73d594c8e
thienphanexcalibur <thienkphan@gmail.com>
2019-11-11 07:49:26 +0700

    Indent

--- proxyme.js
+++ proxyme.js
@@ -1,8 +1,7 @@
 const Proxy = require('http-mitm-proxy');
 const proxy = Proxy();
 const {
-	spawn,
-	exec
+  spawn
 } = require('child_process');
 const fs = require('fs');
 const http = require('http');
@@ -10,30 +9,32 @@ const path = require('path');
 const chalk = require('chalk');
 let plugins = null;
 const pluginPath = path.resolve(process.cwd(), 'plugins');
+
 // Check if plugin paths exists then make a closure ;)
 if (fs.existsSync(pluginPath)) {
-	console.log(chalk.green('Your plugin path', pluginPath));
-	plugins = require(pluginPath);
+  console.log(chalk.green('Your plugin path', pluginPath));
+  plugins = require(pluginPath);
 }
+
 module.exports = function proxyMe(args) {
   const {
-		publicPath = process.cwd(),
-    pac,
-    proxyHost,
-    proxyPort,
-    debugHost,
-    debugPort,
-		rules,
-		certDir
+      publicPath = process.cwd(),
+      pac,
+      proxyHost,
+      proxyPort,
+      debugHost,
+      debugPort,
+      rules,
+      certDir
   } = args;
 
-	if (certDir) {
-		console.log(chalk.bgWhite.black('Your certificate directory:', certDir));
-	}
+  if (certDir) {
+    console.log(chalk.bgWhite.black('Your certificate directory:', certDir));
+  }
 
-	/* Setup debug server 
-		* to display all traffics from proxy */
-    const debugServer = http.createServer((req, res) => {
+  /* Setup debug server
+   * to display all traffics from proxy */
+  const debugServer = http.createServer((req, res) => {
     const staticBasePath = __dirname;
     const resolvedBase = path.resolve(staticBasePath);
     const safeSuffix = path.normalize(req.url).replace(/^(\.\.[\/\\])+/, '');
@@ -46,17 +47,17 @@ module.exports = function proxyMe(args) {
       fileLoc = path.join(fileLoc, 'index.html');
     }
 
-    fs.readFile(fileLoc, function(err, data) {
-        if (err) {
-            res.writeHead(404, 'Not Found');
-            res.write('404: File Not Found!');
-            return res.end();
-        }
-
-        res.statusCode = 200;
-
-        res.write(data);
+    fs.readFile(fileLoc, function (err, data) {
+      if (err) {
+        res.writeHead(404, 'Not Found');
+        res.write('404: File Not Found!');
         return res.end();
+      }
+
+      res.statusCode = 200;
+
+      res.write(data);
+      return res.end();
     });
   });
 
@@ -69,20 +70,20 @@ module.exports = function proxyMe(args) {
 
   const io = require('socket.io')(debugServer);
 
-/* Inject global PAC file for determine proxy traffics */
+  /* Inject global PAC file for determine proxy traffics */
   spawn('bash', ['attach.sh', '--pac', pac], {
     cwd: path.join(__dirname, 'scripts')
   });
 
-	if (certDir) {
-		proxy.onCertificateRequired = function(hostname, callback) {
-			console.log(hostname);
-				return callback(null, {
-					keyFile: path.resolve(certDir, `${hostname}-key.pem`),
-					certFile: path.resolve(certDir, `${hostname}-cert.pem`)
-				});
-		};
-	}
+  if (certDir) {
+    proxy.onCertificateRequired = function (hostname, callback) {
+      console.log(hostname);
+      return callback(null, {
+        keyFile: path.resolve(certDir, `${hostname}-key.pem`),
+        certFile: path.resolve(certDir, `${hostname}-cert.pem`)
+      });
+    };
+  }
 
   proxy.onRequest(function (ctx, callback) {
     ctx.use(Proxy.gunzip);
@@ -91,7 +92,7 @@ module.exports = function proxyMe(args) {
     const host = ctx.clientToProxyRequest.headers.host;
     // Log
     console.log(remoteAddress, ' requests ', ctx.clientToProxyRequest.url);
-   
+
     // Transport to socket
     io.emit('request', `${remoteAddress} requests ${url}`)
 
@@ -103,25 +104,28 @@ module.exports = function proxyMe(args) {
     let hostMatched = rules[host];
     // Add more logic here
     // [NOTICE] Currently support only level 1 path
-		if (hostMatched) {
+    if (hostMatched) {
       let [mapPaths, hostMapping] = hostMatched;
-			for(iterPath in mapPaths) {
-				const [mapPath, pathMapping] = mapPaths[iterPath];
-				if (url.match(new RegExp(iterPath))) {
-					ctx.proxyToServerRequestOptions.path = ctx.proxyToServerRequestOptions.path.replace(`/${iterPath}`, '');
-					ctx.proxyToServerRequestOptions.host = pathMapping.host;
-					ctx.proxyToServerRequestOptions.port = pathMapping.port;
-		// Run plugin after this sections
-					if (plugins) {
-							plugins.call(this, ctx);
-					}
-					return callback();
-				}
-			}
-			if (hostMapping && hostMapping instanceof Object) {
-				ctx.proxyToServerRequestOptions.host = hostMapping.host;
-				ctx.proxyToServerRequestOptions.port = hostMapping.port;
-			}
+      for (iterPath in mapPaths) {
+        /*
+            TODO: running an excursion here to map all subpaths to their hosts and ports
+        */
+        const [mapPath, pathMapping] = mapPaths[iterPath];
+        if (url.match(new RegExp(iterPath))) {
+          ctx.proxyToServerRequestOptions.path = ctx.proxyToServerRequestOptions.path.replace(`/${iterPath}`, '');
+          ctx.proxyToServerRequestOptions.host = pathMapping.host;
+          ctx.proxyToServerRequestOptions.port = pathMapping.port;
+          // Run plugin after this sections
+          if (plugins) {
+            plugins.call(this, ctx);
+          }
+          return callback();
+        }
+      }
+      if (hostMapping && hostMapping instanceof Object) {
+        ctx.proxyToServerRequestOptions.host = hostMapping.host;
+        ctx.proxyToServerRequestOptions.port = hostMapping.port;
+      }
     }
     return callback();
   });
@@ -140,7 +144,7 @@ module.exports = function proxyMe(args) {
   proxy.onError((ctx, err, errorKind) => {
     console.log(err);
     console.log(errorKind);
-  })
+  });
 
   proxy.listen({
     host: proxyHost,
